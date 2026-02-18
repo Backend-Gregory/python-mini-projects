a98c6d1f0cc2ff262eafbeb7612c1005898d5592
Frontend-Gregory <okishevgriha@gmail.com>
2026-02-18 22:38:06 +0300

    Implement add entry with datetime and save to file

--- 15_file_diary/main.py
+++ 15_file_diary/main.py
@@ -1,8 +1,15 @@
+from datetime import datetime
 while True:
     print('1. Записать новую запись')
     print('2. Показать все записи')
     print('3. Найти запись по дате')
     print('4. Выход')
     num = int(input('Выберите действие(1-4): '))
-    if num == 4:
+    if num == 1:
+        with open('diary.txt', 'a', encoding='utf-8') as diary:
+            now = datetime.now()
+            now_f = now.strftime('%Y-%m-%d %H:%M')
+            record = input('Введите запись: ')
+            diary.write(f"[{now_f}] {record}")
+    elif num == 4:
         break
