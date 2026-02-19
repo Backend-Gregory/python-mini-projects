9c347d62225b0a69bfc197384e8c91f10c438e4f
Frontend-Gregory <okishevgriha@gmail.com>
2026-02-19 22:19:29 +0300

    Add registration with file saving

---
+++ 01_auth_system/main.py
@@ -0,0 +1,11 @@
+print('=' * 45)
+print('ДОБРО ПОЖАЛОВАТЬ!')
+print('=' * 45)
+print('Это ваш первый запуск.')
+print('Необходимо зарегистрироваться.')
+print()
+with open('auth.txt', 'a', encoding='utf-8') as auth:
+    login = input('Придумайте логин: ')
+    password = input('Придумайте пароль: ')
+    auth.write(login + '\n')
+    auth.write(password)
