d225ee0babc666c15ab959ac9b32cec3095c1457
Frontend-Gregory <okishevgriha@gmail.com>
2026-02-19 22:33:13 +0300

    Add first run check with registration

--- 01_auth_system/main.py
+++ 01_auth_system/main.py
@@ -1,11 +1,17 @@
-print('=' * 45)
-print('ДОБРО ПОЖАЛОВАТЬ!')
-print('=' * 45)
-print('Это ваш первый запуск.')
-print('Необходимо зарегистрироваться.')
-print()
-with open('auth.txt', 'a', encoding='utf-8') as auth:
-    login = input('Придумайте логин: ')
-    password = input('Придумайте пароль: ')
-    auth.write(login + '\n')
-    auth.write(password)
+import os
+if os.path.exists('auth.txt'):
+    print('=' * 45)
+    print('ВХОД В СИСТЕМУ')
+    print('=' * 45)
+else:
+    print('=' * 45)
+    print('ДОБРО ПОЖАЛОВАТЬ!')
+    print('=' * 45)
+    print('Это ваш первый запуск.')
+    print('Необходимо зарегистрироваться.')
+    print()
+    with open('auth.txt', 'a', encoding='utf-8') as auth:
+        login = input('Придумайте логин: ')
+        password = input('Придумайте пароль: ')
+        auth.write(login + '\n')
+        auth.write(password)
