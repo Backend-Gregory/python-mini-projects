b5631eafe036c225f36b66d756ebac8c8eb1dbf5
Frontend-Gregory <okishevgriha@gmail.com>
2026-02-20 21:30:16 +0300

    Add time-based lockout after 3 attempts

--- 01_auth_system/main.py
+++ 01_auth_system/main.py
@@ -1,42 +1,65 @@
 import os
+import time
 
-if os.path.exists('auth.txt'):
-    print('=' * 45)
+BLOCK_TIME = 900
+AUTH_FILE = 'auth.txt'
+LOCK_FILE = 'lockout.txt'
+LINE_WIDTH = '=' * 45
+
+if os.path.exists(AUTH_FILE):
+    print(LINE_WIDTH)
     print('ВХОД В СИСТЕМУ')
-    print('=' * 45)
-    if os.path.exists('status.txt'):
-        print('Вы заблокированы! Попыток не осталось.')
-    else:
-        with open('auth.txt', encoding='utf-8') as auth:
-            attempts = 3
-            while True:
-                login = input('Логин: ')
-                password = input('Пароль: ')
-                status = None
-                auth.seek(0)
-                if login == auth.readline().strip() and \
-                password == auth.readline().strip():
-                    status = True
-                    print('Добро пожаловать!')
-                    break
+    print(LINE_WIDTH)
+
+    if os.path.exists(LOCK_FILE):
+        with open(LOCK_FILE, encoding='utf-8') as lock:
+            unlock_time = float(lock.read())
+
+        if time.time() < unlock_time:
+            time_ost = int((unlock_time - time.time()) / 60)
+            print(f'До разблокировки осталось {time_ost} минут')
+            exit()
+        else:
+            os.remove(LOCK_FILE)
+
+    with open(AUTH_FILE, encoding='utf-8') as auth:
+        attempts = 3
+        while True:
+            login = input('Логин: ')
+            password = input('Пароль: ')
+            status = None
+            auth.seek(0)
+
+            if login == auth.readline().strip() and \
+            password == auth.readline().strip():
+                status = True
+                print(f"Статус: {status}")
+                print('Добро пожаловать!')
+                break
+
+            else:
+                if attempts > 0:
+                    status = False
+                    print(f"Статус: {status}")
+                    print(f'Неверно! Осталось {attempts} попыток.')
+                    attempts -= 1
+
                 else:
-                    if attempts > 0:
-                        status = False
-                        print(f'Неверно! Осталось {attempts} попыток.')
-                        attempts -= 1
-                    else:
-                        with open('status.txt', 'w', encoding='utf-8'):
-                            print('Вы заблокированы! Попыток больше не осталось.')
-                            break
+                    unlock_time = time.time() + BLOCK_TIME
+                    with open(LOCK_FILE, 'w', encoding='utf-8') as lockout:
+                        lockout.write(str(unlock_time))
+                        print(f'Попыток не осталось! Вы заблокированы на 15 минут.')
+                            
             
 else:
-    print('=' * 45)
+    print(LINE_WIDTH)
     print('ДОБРО ПОЖАЛОВАТЬ!')
-    print('=' * 45)
+    print(LINE_WIDTH)
     print('Это ваш первый запуск.')
     print('Необходимо зарегистрироваться.')
     print()
-    with open('auth.txt', 'a', encoding='utf-8') as auth:
+    
+    with open(AUTH_FILE, 'a', encoding='utf-8') as auth:
         login = input('Придумайте логин: ')
         password = input('Придумайте пароль: ')
         auth.write(login + '\n')
