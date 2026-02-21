40af4ccbeed3f9ac7084fc78b49089e2100c5c52
Frontend-Gregory <okishevgriha@gmail.com>
2026-02-21 23:48:56 +0300

    Add win condition check

--- 02_tic_tac_toe/main.py
+++ 02_tic_tac_toe/main.py
@@ -27,8 +27,29 @@ def print_pole(pole):
         if i < 2:
             print('  -----------')
 
-n, m = 3, 3
-pole = [['.' for _ in range(m)] for _ in range(n)]
+def check_winner(board):
+    win_combinations = [
+        [(0,0), (0,1), (0,2)],
+        [(1,0), (1,1), (1,2)],
+        [(2,0), (2,1), (2,2)],
+        [(0,0), (1,0), (2,0)],
+        [(0,1), (1,1), (2,1)],
+        [(0,2), (1,2), (2,2)],
+        [(0,0), (1,1), (2,2)],
+        [(0,2), (1,1), (2,0)]
+    ]
+
+    for combo in win_combinations:
+        a, b, c = combo
+        if board[a[0]][a[1]] == board[b[0]][b[1]] == board[c[0]][c[1]] != '.':
+            return board[a[0]][a[1]]
+        
+    return None
+
+
+
+
+pole = [['.' for _ in range(3)] for _ in range(3)]
 
 print_pole(pole)
 
@@ -37,5 +58,9 @@ while True:
     pole[row][col] = current_player
     print_pole(pole)
     current_player = 'O' if current_player == 'X' else 'X'
-        
+    
+    winner = check_winner(pole)
+    if winner != None:
+        print(f'Победитель {winner}')
+        break
     
