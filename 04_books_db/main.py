6d180e6fa40e2e9f4b3fa60ff0c978aca2b262e5
Frontend-Gregory <okishevgriha@gmail.com>
2026-02-25 21:03:44 +0300

    Implement add book feature with file save

--- 04_books_db/main.py
+++ 04_books_db/main.py
@@ -1,4 +1,10 @@
 LINE_WIDTH = 45
+LIBRARY = 'library.txt'
+
+def add_book(book, author, year):
+    with open(LIBRARY, 'a', encoding='utf-8') as library:
+        library.write(f'{book}|{author}|{year}\n')
+
 print("=" * LINE_WIDTH)
 print('База данных "Книги"')
 print('=' * LINE_WIDTH)
@@ -8,3 +14,9 @@ print('2. Поиск по автору')
 print('3. Сортировка по году')
 print('4. Показать все')
 num = int(input('Выбери действие (1-4): '))
+if num == 1:
+    book = input('Название книги: ')
+    author = input('Автор книги: ')
+    year = input('Год выпуска: ')
+    add_book(book, author, year)
+    print('✅ Книга добавлена!')
