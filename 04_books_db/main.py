a79009ff4b5b19c065da657ca42d359eed19e640
Frontend-Gregory <okishevgriha@gmail.com>
2026-02-25 23:10:06 +0300

    Add menu loop

--- 04_books_db/main.py
+++ 04_books_db/main.py
@@ -26,33 +26,38 @@ def get_all_books():
 print("=" * LINE_WIDTH)
 print('База данных "Книги"')
 print('=' * LINE_WIDTH)
-print()
-print('1. Добавить книгу')
-print('2. Поиск по автору')
-print('3. Сортировка по году')
-print('4. Показать все')
-num = int(input('Выбери действие (1-4): '))
-if num == 1:
-    book = input('Название книги: ')
-    author = input('Автор книги: ')
-    year = input('Год выпуска: ')
-    add_book(book, author, year)
-    print('✅ Книга добавлена!')
-elif num == 2:
-    author = input('Автор книги: ')
-    books = search_by_author(author)
-    if books:
-        print()
-        print(f'Книги автора {author}:')
-        for i in books:
-            print(f"{i[0]} ({i[1]} год)")
+while True:
+    print()
+    print('1. Добавить книгу')
+    print('2. Поиск по автору')
+    print('3. Сортировка по году')
+    print('4. Показать все')
+
+    num = int(input('Выбери действие (1-4): '))
+    if num == 1:
+        book = input('Название книги: ')
+        author = input('Автор книги: ')
+        year = input('Год выпуска: ')
+        add_book(book, author, year)
+        print('✅ Книга добавлена!')
+    elif num == 2:
+        author = input('Автор книги: ')
+        books = search_by_author(author)
+        if books:
+            print()
+            print(f'Книги автора {author}:')
+            for i in books:
+                print(f"{i[0]} ({i[1]} год)")
+        else:
+            print(f'Книги автора {author} не найдены.')
     else:
-        print(f'Книги автора {author} не найдены.')
-else:
-    books = get_all_books()
-    if num == 3:
-        books = sorted(books, key=lambda x: x[2])
-        print()
-        print("Все книги сортированые по году:")
-    for book in books:
-        print(f"{book[0]} - {book[1]} ({book[2]} год)")
+        books = get_all_books()
+        if num == 3:
+            books = sorted(books, key=lambda x: x[2])
+            print()
+            print("Все книги сортированые по году:")
+        for book in books:
+            print(f"{book[0]} - {book[1]} ({book[2]} год)")
+    inp = input('Хотите продолжить? (да/нет)').lower()
+    if inp == 'нет':
+        break
