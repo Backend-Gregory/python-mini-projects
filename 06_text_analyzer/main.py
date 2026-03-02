72c90341cb74ed9e7420aa06e863c35ddbe96557
Frontend-Gregory <okishevgriha@gmail.com>
2026-03-02 21:52:40 +0300

    Add print function

--- 06_text_analyzer/main.py
+++ 06_text_analyzer/main.py
@@ -7,14 +7,22 @@ def del_punctuation(value):
         value = value.replace(punct, '')
     return value
 
-def clean(text):
-    clean_text = {del_punctuation(word).lower() for word in text.split() if len(word) > 1}
-    return clean_text
+def clean(text, as_set=True):
+    clean_text = [del_punctuation(word).lower() for word in text.split() if len(word) > 1]
+    return set(clean_text) if as_set else clean_text
 
 def count_pred(text):
     text = text.replace('...', '.')
     count_pred = text.count('.') + text.count('!') + text.count('?')
     return count_pred
+
+def print_res(count_pred, count_word, count_sim, unique_word_text):
+    print('Информация для 1 текста:')
+    print(f'Всего предложений: {count_pred}')
+    print(f'Всего слов: {count_word}')
+    print(f'Всего символов: {count_sim}')
+    print(f'Всего уникальных слов: {len(unique_word_text)}')
+    print('Уникальные слова:', *unique_word_text if unique_word_text else ['не найдено'])
 
 print('=' * LINE_WIDTH)
 print('АНАЛИЗАТОР ТЕКСТА')
@@ -24,10 +32,18 @@ print()
 text1 = input('Введите первый текст:\n')
 print()
 text2 = input('Введите второй текст:\n')
+
 set_text1 = clean(text1)
 set_text2 = clean(text2)
 count_pred_text1 = count_pred(text1)
 count_pred_text2 = count_pred(text2)
+count_word1 = len(clean(text1, False))
+count_word2 = len(clean(text2, False))
+count_sim1 = len(text1)
+count_sim2 = len(text2)
+unique_word = set_text1 & set_text2
+unique_word_text1 = set_text1 - set_text2
+unique_word_text2 = set_text2 - set_text1
 
 print()
 print('=' * LINE_WIDTH)
@@ -36,18 +52,10 @@ print('=' * LINE_WIDTH)
 print()
 print('Общая информация:')
 
-unique_word = set_text1 & set_text2
-
 if unique_word:
     print('Общие слова:', *sorted(unique_word), sep=', ')
 else:
     print('Общих слов не найдено.')
 
-print()
-print('Информация для 1 текста:')
-print(f'Всего предложений в первом тексте: {count_pred_text1}')
-print(f'Всего символов в первом тексте: {len(text1)}')
-print()
-print('Информация для 2 текста:')
-print(f'Всего предложений во втором тексте: {count_pred_text2}')
-print(f'Всего символов во втором тексте: {len(text2)}')
+print_res(count_pred_text1, count_word1, count_sim1, unique_word_text1)
+print_res(count_pred_text2, count_word2, count_sim2, unique_word_text2)
