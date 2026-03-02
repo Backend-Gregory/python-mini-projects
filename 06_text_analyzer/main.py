c6852abcfa9606b2a624bad2127412261fe225ca
Frontend-Gregory <okishevgriha@gmail.com>
2026-03-02 20:43:16 +0300

    Fix common words output

--- 06_text_analyzer/main.py
+++ 06_text_analyzer/main.py
@@ -1,4 +1,16 @@
+from string import punctuation
+
 LINE_WIDTH = 45
+
+def del_punctuation(value):
+    for punct in punctuation:
+        value = value.replace(punct, '')
+    return value
+
+def clean(text):
+    clean_text = {del_punctuation(word).lower() for word in text.split() if len(word) > 1}
+    return clean_text
+
 print('=' * LINE_WIDTH)
 print('АНАЛИЗАТОР ТЕКСТА')
 print('=' * LINE_WIDTH)
@@ -7,8 +19,8 @@ print()
 text1 = input('Введите первый текст:\n')
 print()
 text2 = input('Введите второй текст:\n')
-set_text1 = set(text1.split())
-set_text2 = set(text2.split())
+set_text1 = clean(text1)
+set_text2 = clean(text2)
 
 print()
 print(f'Всего символов в первом тексте: {len(text1)}')
