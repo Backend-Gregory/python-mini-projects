5c7e11cde2b7031ef2fb58118b54a04361344b3d
Frontend-Gregory <okishevgriha@gmail.com>
2026-03-02 21:15:08 +0300

    Add sentence count feature

--- 06_text_analyzer/main.py
+++ 06_text_analyzer/main.py
@@ -11,6 +11,11 @@ def clean(text):
     clean_text = {del_punctuation(word).lower() for word in text.split() if len(word) > 1}
     return clean_text
 
+def count_pred(text):
+    text = text.replace('...', '.')
+    count_pred = text.count('.') + text.count('!') + text.count('?')
+    return count_pred
+
 print('=' * LINE_WIDTH)
 print('АНАЛИЗАТОР ТЕКСТА')
 print('=' * LINE_WIDTH)
@@ -21,10 +26,15 @@ print()
 text2 = input('Введите второй текст:\n')
 set_text1 = clean(text1)
 set_text2 = clean(text2)
+count_pred_text1 = count_pred(text1)
+count_pred_text2 = count_pred(text2)
 
 print()
-print(f'Всего символов в первом тексте: {len(text1)}')
-print(f'Всего символов во втором тексте: {len(text2)}')
+print('=' * LINE_WIDTH)
+print('РЕЗУЛЬТАТЫ АНАЛИЗА')
+print('=' * LINE_WIDTH)
+print()
+print('Общая информация:')
 
 unique_word = set_text1 & set_text2
 
@@ -32,3 +42,12 @@ if unique_word:
     print('Общие слова:', *sorted(unique_word), sep=', ')
 else:
     print('Общих слов не найдено.')
+
+print()
+print('Информация для 1 текста:')
+print(f'Всего предложений в первом тексте: {count_pred_text1}')
+print(f'Всего символов в первом тексте: {len(text1)}')
+print()
+print('Информация для 2 текста:')
+print(f'Всего предложений во втором тексте: {count_pred_text2}')
+print(f'Всего символов во втором тексте: {len(text2)}')
