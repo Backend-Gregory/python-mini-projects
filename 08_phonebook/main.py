d1e7018d22173cc447fc4c75e18b6594be1149a0
Frontend-Gregory <okishevgriha@gmail.com>
2026-03-08 12:24:24 +0300

    Add add contact feature with JSON storage

--- 08_phonebook/main.py
+++ 08_phonebook/main.py
@@ -1,6 +1,24 @@
+import json
+import os
+
+FILE = "contacts.json"
+
+if os.path.exists(FILE):
+    with open(FILE, "r", encoding="utf-8") as f:
+        contacts = json.load(f)
+else:
+    contacts = []
+
 print("1. Добавить контакт")
 print("2. Найти по имени")
 print("3. Найти по номеру")
 print("4. Показать все контакты")
 print("5. Выход")
 num = int(input("Выберите действие: "))
+
+if num == 1:
+    contact = {"name": input("Имя контакта: "), "number": input("Номер контакта: ")}
+    contacts.append(contact)
+    with open(FILE, "w", encoding="utf-8") as f:
+        json.dump(contacts, f, ensure_ascii=False, indent=2)
+    print("Контакт сохранен")
