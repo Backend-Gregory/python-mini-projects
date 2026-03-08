38138e87edf7494cdfa70e3f15b3d158e3558872
Frontend-Gregory <okishevgriha@gmail.com>
2026-03-08 12:51:59 +0300

    Add find by name function

--- 08_phonebook/main.py
+++ 08_phonebook/main.py
@@ -9,6 +9,35 @@ if os.path.exists(FILE):
 else:
     contacts = []
 
+def add_contact(contacts):
+    name = input("Имя контакта: ")
+    number = input("Номер контакта: ")
+    contact = {"name": name, "number": number}
+    contacts.append(contact)
+
+    with open(FILE, "w", encoding="utf-8") as f:
+        json.dump(contacts, f, ensure_ascii=False, indent=2)
+    print("Контакт сохранен")
+
+def find_by_name(contacts):
+    name = input("Введите имя: ")
+    if contacts:
+        for ch in contacts:
+            if ch['name'] == name:
+                print(f'{name}: {ch['number']}')
+                break
+        else:
+            print('Контакт не найден')
+    else:
+        print("У вас нет ни одного контакта.")
+
+def print_contacts(contacts):
+    if contacts:
+        for ch in contacts:
+            print(f'{ch['name']}: {ch['number']}')
+    else:
+        print("У вас нет ни одного контакта.")
+
 print("1. Добавить контакт")
 print("2. Найти по имени")
 print("3. Найти по номеру")
@@ -17,15 +46,10 @@ print("5. Выход")
 num = int(input("Выберите действие: "))
 
 if num == 1:
-    contact = {"name": input("Имя контакта: "), "number": input("Номер контакта: ")}
-    contacts.append(contact)
-    with open(FILE, "w", encoding="utf-8") as f:
-        json.dump(contacts, f, ensure_ascii=False, indent=2)
-    print("Контакт сохранен")
+    add_contact(contacts)
+
+elif num == 2:
+    find_by_name(contacts)
 
 elif num == 4:
-    if contacts:
-        for ch in contacts:
-            print(f'{ch['name']}: {ch['number']}')
-    else:
-        print("У вас нет ни одного контакта.")
+    print_contacts(contacts)
