d0f568fb83162d1c937a0af2604bb64c745d578d
Frontend-Gregory <okishevgriha@gmail.com>
2026-03-11 21:57:04 +0300

    Add currency conversion function

--- 10_currency_converter/main.py
+++ 10_currency_converter/main.py
@@ -1,7 +1,21 @@
+import requests
+
+def get_rate(base, target, sm):
+    response = requests.get(f"https://api.exchangerate-api.com/v4/latest/{base}")
+    data = response.json()
+    res = data["rates"][target] * sm
+    return res
+
+
 LINE_WIDTH = 45
 print('=' * LINE_WIDTH)
 print('КОНВЕРТЁР ВАЛЮТ')
 print('=' * LINE_WIDTH)
+
 currency1 = input("Введите исходную валюту (например USD): ")
 currency2 = input("Введите целевую валюту (например RUB): ")
 amount = float(input(f"Сколько {currency1} конвертируем в {currency2}?: "))
+
+result = get_rate(currency1, currency2, amount)
+
+print(f"{amount} {currency1} - {result} {currency2}")
