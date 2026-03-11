45110dce1b19eb8f0960030b4ee64a96269082b0
Frontend-Gregory <okishevgriha@gmail.com>
2026-03-11 21:15:07 +0300

    Add menu and currency input

---
+++ 10_currency_converter/main.py
@@ -0,0 +1,6 @@
+LINE_WIDTH = 45
+print('=' * LINE_WIDTH)
+print('КОНВЕРТЁР ВАЛЮТ')
+print('=' * LINE_WIDTH)
+currency1 = input("Введите исходную валюту (например USD): ")
+currency2 = input("Введите целевую валюту (например RUB): ")
