5a085634a0df4411cbaeddd7b2200aa4a6ce262d
Frontend-Gregory <okishevgriha@gmail.com>
2026-03-12 23:02:55 +0300

    Add calculate function for percentages

--- 11_accountant_calc/main.py
+++ 11_accountant_calc/main.py
@@ -1,10 +1,22 @@
+from decimal import Decimal, ROUND_HALF_UP as R
+
+def Calculate(amount, percent):
+    res = amount * (percent / 100)
+    rounded = res.quantize(Decimal("0.01"), rounding=R)
+    print(rounded)
+
 LINE_WIDTH = 45
+
 print('=' * LINE_WIDTH)
 print('БУХГАЛТЕРСКИЙ КАЛЬКУЛЯТОР')
 print('=' * LINE_WIDTH)
 print()
-print('1. Рассчитать НДФЛ (13%)')
-print('2. Рассчитать страховые взносы (30%)')
-print('3. Рассчитать сумму "грязными"')
+print('1. Рассчитать НДФЛ (13%): ')
+print('2. Рассчитать страховые взносы (30%): ')
+print('3. Рассчитать сумму "грязными" (указывай чистыми): ')
 print('4. Выход')
-num = int(input('Выберите действие (1-4):'))
+num = int(input('Выберите действие (1-4): '))
+if num == 1:
+    amount = Decimal(input("Введите зарплату (грязными): "))
+    percent = Decimal('13')
+    Calculate(amount, percent)
