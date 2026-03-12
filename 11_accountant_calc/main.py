45a8d7b2253e5c057b3a3864006e2fea1157273b
Frontend-Gregory <okishevgriha@gmail.com>
2026-03-13 00:01:05 +0300

    Add main loop

--- 11_accountant_calc/main.py
+++ 11_accountant_calc/main.py
@@ -15,15 +15,19 @@ LINE_WIDTH = 45
 print('=' * LINE_WIDTH)
 print('БУХГАЛТЕРСКИЙ КАЛЬКУЛЯТОР')
 print('=' * LINE_WIDTH)
-print()
-print('1. Рассчитать НДФЛ (13%): ')
-print('2. Рассчитать страховые взносы (30%): ')
-print('3. Рассчитать сумму "грязными" (указывай чистыми): ')
-print('4. Выход')
-num = int(input('Выберите действие (1-4): '))
-if num == 1:
-    get_amount_and_calculate('13')
-elif num == 2:
-    get_amount_and_calculate('30')
-elif num == 3:
-    get_amount_and_calculate('13', "чистыми")
+while True:
+    print()
+    print('1. Рассчитать НДФЛ (13%): ')
+    print('2. Рассчитать страховые взносы (30%): ')
+    print('3. Рассчитать сумму "грязными" (указывай чистыми): ')
+    print('4. Выход')
+
+    num = int(input('Выберите действие (1-4): '))
+    if num == 1:
+        get_amount_and_calculate('13')
+    elif num == 2:
+        get_amount_and_calculate('30')
+    elif num == 3:
+        get_amount_and_calculate('13', "чистыми")
+    elif num == 4:
+        break
