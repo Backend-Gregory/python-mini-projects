e590e46201179430ca1fb60255f0c15f6b433a55
Frontend-Gregory <okishevgriha@gmail.com>
2026-03-15 21:21:31 +0300

    Add file error handling to logger

--- 13_debug_decorator/main.py
+++ 13_debug_decorator/main.py
@@ -10,13 +10,17 @@ def log_calls(func):
         end = time.time() - start
         name = func.__name__
 
-
-        with open('log.txt', 'a', encoding='utf-8') as log:
-            log.write(f'Дата: {data}\n')
-            log.write(f'Время вызова декоратора: {time_now}\n')
-            log.write(f'→ {name} {args, kwargs}\n')
-            log.write(f'← Результат: {res}\n')
-            log.write(f'⏱ Время выполнения: {end:.4f} сек\n\n')
+        try:
+            with open('log.txt', 'a', encoding='utf-8') as log:
+                log.write(f'Дата: {data}\n')
+                log.write(f'Время вызова декоратора: {time_now}\n')
+                log.write(f'→ {name} {args, kwargs}\n')
+                log.write(f'← Результат: {res}\n')
+                log.write(f'⏱ Время выполнения: {end:.4f} сек\n\n')
+        except (IOError, PermissionError) as e:
+            print(f"⚠️ Не удалось записать лог: {e}")
+        except Exception as e:
+            print(f'Неизвестная ошибка: {e}')
 
         return res
     return wrapper
