f30814d169e17457446a1e4c10155f062761fb24
Frontend-Gregory <okishevgriha@gmail.com>
2026-03-15 21:20:11 +0300

    Add date and time to log output

--- 13_debug_decorator/main.py
+++ 13_debug_decorator/main.py
@@ -1,12 +1,19 @@
 import time
+from datetime import datetime
 def log_calls(func):
     def wrapper(*args, **kwargs):
+        now = datetime.now()
+        data = now.strftime('%Y-%m-%d')
+        time_now = now.strftime('%H:%M')
         start = time.time()
         res = func(*args, **kwargs)
         end = time.time() - start
         name = func.__name__
 
+
         with open('log.txt', 'a', encoding='utf-8') as log:
+            log.write(f'Дата: {data}\n')
+            log.write(f'Время вызова декоратора: {time_now}\n')
             log.write(f'→ {name} {args, kwargs}\n')
             log.write(f'← Результат: {res}\n')
             log.write(f'⏱ Время выполнения: {end:.4f} сек\n\n')
