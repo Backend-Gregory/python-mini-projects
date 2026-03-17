bc1d3bac2340232d5797068dcd3f318835efb984
Frontend-Gregory <okishevgriha@gmail.com>
2026-03-18 00:29:47 +0300

    Add status statistics to log analyzer

--- 14_analyzer_log/main.py
+++ 14_analyzer_log/main.py
@@ -4,12 +4,16 @@ LINE_WIDTH = 45
 FILE = "access.log"
 
 unique_ip = Counter()
+status_counter = Counter()
 
 with open(FILE, encoding="utf-8") as logs:
     for count, log in enumerate(logs, 1):
         parts = log.split()
         ip = parts[0]
+        status = parts[-2]
+
         unique_ip[ip] += 1
+        status_counter[status] += 1
 
 print("=" * LINE_WIDTH)
 print("ЛОГ-АНАЛИЗАТОР")
@@ -17,3 +21,9 @@ print("=" * LINE_WIDTH)
 print()
 print(f"Всего запросов: {count}")
 print(f"Уникальных IP-адресов: {len(unique_ip)}")
+print()
+print("СТАТИСТИКА ПО СТАТУСАМ:")
+print(f"✅ Успешных (200): {status_counter['200']}")
+print(f"⚠️ Перенаправлений (301, 302): {status_counter['301'] + status_counter['302']}")
+print(f"❌ Ошибок клиента (404, 403): {status_counter['404'] + status_counter['403']}")
+print(f"💥 Ошибок сервера (500): {status_counter['500']}")
