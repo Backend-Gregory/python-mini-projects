76fd3b7b4b7513f3555350270815cdb440807c61
Frontend-Gregory <okishevgriha@gmail.com>
2026-03-19 19:23:15 +0300

    Add get_info method to Movie

--- 16_oop_movie_catalog/main.py
+++ 16_oop_movie_catalog/main.py
@@ -8,3 +8,6 @@ class Movie:
     
     def add_similar(self, movie):
         self.similar.append(movie)
+    
+    def get_info(self):
+        return f'{self.title} ({self.year}) - {self.genre}, Рейтинг: {self.rating}'
