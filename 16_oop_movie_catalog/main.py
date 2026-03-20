1d57cbf8b5230b475de66ed650c8591652a5e33b
Frontend-Gregory <okishevgriha@gmail.com>
2026-03-20 22:36:34 +0300

    Add by_genre method in Recommender class

--- 16_oop_movie_catalog/main.py
+++ 16_oop_movie_catalog/main.py
@@ -1,3 +1,11 @@
+import json
+from collections import Counter
+import os
+
+FILE_MOVIES = 'movies.json'
+FILE_USERS = 'users.json'
+MIN_COUNT = 3
+
 class Movie:
     def __init__(self, title, year, genre, rating):
         self.title = title
@@ -25,3 +33,24 @@ class Recommender:
     @staticmethod
     def top_rated(all_movies, n):
         return sorted(all_movies, key=lambda m: m.rating, reverse=True)[:n]
+    
+    @staticmethod
+    def by_genre(watched, all_movies):
+        count_genre = Counter()
+        sum_rating_genre = {}
+        for movie in watched:
+            genre = movie.genre
+            sum_rating_genre[genre] = sum_rating_genre.get(genre, 0) + watched[movie]
+            count_genre[genre] += 1
+        
+        if not sum_rating_genre:
+            return []
+        
+        favorite = max(sum_rating_genre, key=lambda g: sum_rating_genre[g] / count_genre[g] if count_genre[g] >= MIN_COUNT else 0)
+
+        candidates = [
+            m for m in all_movies
+            if m.genre == favorite and m not in watched
+        ]
+
+        return sorted(candidates, key=lambda m: m.rating, reverse=True)[:3]
