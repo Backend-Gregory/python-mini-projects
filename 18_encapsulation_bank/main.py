345bfe1b9f87e0e8cf7e2813f2f2c0448ea270fd
Frontend-Gregory <okishevgriha@gmail.com>
2026-03-22 23:11:27 +0300

    Add getters and deposit method with validation

--- 18_encapsulation_bank/main.py
+++ 18_encapsulation_bank/main.py
@@ -2,3 +2,17 @@ class BankAccount:
     def __init__(self, owner):
         self.__owner = owner
         self.__balance = 0
+    
+    @property
+    def balance(self):
+        return self.__balance
+    
+    @property
+    def owner(self):
+        return self.__owner
+
+    def deposit(self, amount):
+        if amount <= 0:
+            raise ValueError("Сумма пополнения должна быть больше нуля")
+        self.__balance += amount
+        print('Деньги успешно внесены')
