4a7ed2f0451b48525fe936149d0b39c0b29a06e7
Frontend-Gregory <okishevgriha@gmail.com>
2026-03-23 21:58:14 +0300

    Add Circle class with area and info

--- 19_polymorphism_shapes/main.py
+++ 19_polymorphism_shapes/main.py
@@ -1,6 +1,18 @@
+from math import pi
 class Shape():
     def area(self):
         return 0
     
     def info(self):
         return 'Фигура'
+    
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+    
+    def area(self):
+        res = pi * (self.radius ** 2)
+        return res
+    
+    def info(self):
+        return 'Круг'
