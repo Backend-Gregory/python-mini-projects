228278d260ab0ccd81c146d873f1fa815a912346
Frontend-Gregory <okishevgriha@gmail.com>
2026-03-23 23:21:43 +0300

    Complete project 19: polymorphism with interactive menu

--- 19_polymorphism_shapes/main.py
+++ 19_polymorphism_shapes/main.py
@@ -26,7 +26,7 @@ class Rectangle(Shape):
         res = self.width * self.height
         return res
     
-    def info():
+    def info(self):
         return 'Прямоугольник'
     
 class Triangle(Shape):
@@ -43,3 +43,33 @@ class Triangle(Shape):
 
 def print_area(obj):
     print(f'{obj.info()}: площадь: {obj.area()}')
+
+def valid(text_value, e=float):
+    while True:
+        try:
+            value = e(input(text_value))
+            return value
+        except ValueError:
+            print('Введите число')
+while True:
+    print('\n1. Круг')
+    print('2. Прямоугольник')
+    print('3. Треугольник')
+    print('4. Выход')
+    num = valid('Выберите фигуру (1-4): ', int)
+    if num == 1:
+        radius = valid('Введите радиус: ')
+        obj = Circle(radius)
+        print_area(obj)
+    elif num == 2:
+        width = valid('Введите ширину: ')
+        height = valid('Введите высоту: ')
+        obj = Rectangle(width, height)
+        print_area(obj)
+    elif num == 3:
+        base = valid('Введите длинну основания: ')
+        height = valid('Введите высоту: ')
+        obj = Triangle(base, height)
+        print_area(obj)
+    else:
+        break
