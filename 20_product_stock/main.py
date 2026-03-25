929d171a132755e35e309bc4864a8467846e010c
Frontend-Gregory <okishevgriha@gmail.com>
2026-03-25 19:48:11 +0300

    Add __eq__ method with isinstance check

--- 20_product_stock/main.py
+++ 20_product_stock/main.py
@@ -6,3 +6,8 @@ class Product:
 
     def __str__(self):
         return f'Товар: {self.name}, цена: {self.price}, остаток: {self.quantity}'
+    
+    def __eq__(self, other):
+        if not isinstance(other, Product):
+            return NotImplemented
+        return self.name == other.name
