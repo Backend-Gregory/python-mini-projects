903af6811bc56dfb2a8ba2dd12fd1761f8fc4a4d
Frontend-Gregory <okishevgriha@gmail.com>
2026-03-25 19:47:55 +0300

    Add __str__ method

--- 20_product_stock/main.py
+++ 20_product_stock/main.py
@@ -3,3 +3,6 @@ class Product:
         self.name = name
         self.price = price
         self.quantity = quantity
+
+    def __str__(self):
+        return f'Товар: {self.name}, цена: {self.price}, остаток: {self.quantity}'
