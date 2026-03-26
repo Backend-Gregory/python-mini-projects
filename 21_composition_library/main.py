ef0b107a949d44cbd0678b9d453cafefa4fe60d0
Frontend-Gregory <okishevgriha@gmail.com>
2026-03-26 22:32:03 +0300

    Add User class with borrow and return methods

--- 21_composition_library/main.py
+++ 21_composition_library/main.py
@@ -6,3 +6,14 @@ class Book:
 
     def __str__(self):
         return f'{self.title} ({self.author}), год: {self.year}'
+    
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.borrowed = []
+
+    def borrow(self, book):
+        self.borrowed.append(book)
+
+    def __str__(self):
+        return f'{self.name}: {len(self.borrowed)} книг'
