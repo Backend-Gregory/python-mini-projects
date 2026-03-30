b22c4dbf33215fea744fb07b939c77b424451955
Frontend-Gregory <okishevgriha@gmail.com>
2026-03-30 19:50:03 +0300

    Add basic menu structure for sales report

---
+++ 22_sales_reports/main.py
@@ -0,0 +1,10 @@
+LINE_WIDTH = 45
+
+print('=' * LINE_WIDTH)
+print('УЧЁТ ПРОДАЖ')
+print('=' * LINE_WIDTH)
+
+print('\n1. Добавить продажу')
+print('2. Показать отчёт')
+print('3. Выход')
+num = int(input('Выберите действие (1-3): '))
