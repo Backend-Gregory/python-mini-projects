35bac8dfbe160e2406f36e747cb18a3997f12add
Frontend-Gregory <okishevgriha@gmail.com>
2026-03-31 21:13:46 +0300

    Add Sale class with __init__ and __str__

--- 22_sales_reports/main.py
+++ 22_sales_reports/main.py
@@ -1,5 +1,17 @@
+from datetime import datetime
+FILE = 'sale.txt'
 LINE_WIDTH = 45
 
+class Sale:
+    def __init__(self, date, product, category, amount):
+        self.date = date
+        self.product = product
+        self.category = category
+        self.amount = amount
+    
+    def __str__(self):
+        return f'{self.date.strftime("%Y-%m-%d")}|{self.product}|{self.amount}'
+    
 print('=' * LINE_WIDTH)
 print('УЧЁТ ПРОДАЖ')
 print('=' * LINE_WIDTH)
