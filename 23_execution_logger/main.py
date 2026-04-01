d4f373ab4cbe351caa8ee54ff90c249a92b26d0b
Frontend-Gregory <okishevgriha@gmail.com>
2026-04-02 00:09:18 +0300

    Complete project 23: fix time format

--- 23_execution_logger/main.py
+++ 23_execution_logger/main.py
@@ -9,7 +9,7 @@ class logger:
     def __enter__(self):
         self.start = time.time()
         self.date = datetime.strftime(datetime.now(), '%Y-%m-%d')
-        self.time = datetime.strftime(datetime.now(), '%H-%M-%S')
+        self.time = datetime.strftime(datetime.now(), '%H:%M:%S')
         with open(FILE, 'a', encoding='utf-8') as log:
             log.write(f'[{self.date}] НАЧАЛО в {self.time}: {self.name}')
         return self
